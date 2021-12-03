1b1358c361b1516ff22d7f7c3a054c9dda361045
YonghoLee11 <kamsyyongho@naver.com>
2021-12-03 14:53:16 +0900

    Update e_news.py

--- news/e_news.py
+++ news/e_news.py
@@ -7,49 +7,31 @@ from time import sleep
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-# import Config
 
-# options = webdriver.ChromeOptions()
-# # options.add_argument('--headless')
-# # options.add_argument('--no-sandbox')
-# # options.add_argument('--disable-dev-shm-usage')
-# options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36")
-# options.add_argument("verify=False")
-# driver = webdriver.Chrome(executable_path=Config.CONFIG['CHROMEPATH'],options=options)
-
-
-driver = webdriver.Chrome('/Users/gg0/Downloads/chromedriver')
+driver = webdriver.Chrome('C:\chromedriver.exe')
 driver.maximize_window()  
 
-# driver.header_overrides = {
-#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36',
-#     'verify': False
-# }
-
-print(1)
-
-
-
-print(2)
 # 페이지 가져오기(이동)
 driver.get('https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=101')
-sleep(2)
-element =   driver.find_elements_by_id('section_body')
-for ele in element:
-    print(ele.text)
-print(3)
+html_source_code = driver.execute_script("return document.body.innerHTML;")
+html_soup = BeautifulSoup(html_source_code, 'html.parser')
 
-sleep(5)
+##print(html_soup)
 
-# login_form = driver.find_element_by_id('section_body')
+section_body = html_soup.find("div",{"id":"section_body"})
+news_arr = []
+for li in section_body.find_all("li"):
+    news = {}
+    for idx , dts in enumerate(li.find_all("dt")):
+        if idx == 0:
+            continue
+        else:
+            news["title"] = dts.text
+            news["href"] = dts.find("a")["href"]
+    news["content"] = li.find("span",{"class" : "lede"}).text  
+    news["compony"] = li.find("span",{"class" : "writing"}).text    
+    print(news)
+    if news != None:   
+        news_arr.append(news)     
 
-# print(login_form)
-
-
-
-# url = "https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=101"
-# headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'}
-# res = requests.get(url , verify = False , headers = headers)
-# soup = BeautifulSoup(res.text, "html.parser")
-
-# content = soup.find_all("ul")
+sleep(1)
